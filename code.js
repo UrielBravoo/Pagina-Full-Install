0ca69ad8422e737bc31f09b0d9f21b1551bb3b94
UrielBravoo <136418554+UrielBravoo@users.noreply.github.com>
2023-06-18 23:40:18 -0300

    Update code.js

--- code.js
+++ code.js
@@ -13,3 +13,33 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+
+/*
+PROPOSITO: button cambia de modo oscuro a modo claro
+PRECONDICION:ninguna.
+*/
+
+let button = document.getElementById("myButton");
+let titulo = document.getElementById("titulo");
+let texto = document.getElementById("container");
+let isBlack = false;
+
+button.addEventListener("click", function(){
+    if(isBlack === true){
+        document.body.style.background = 'url(fondo.jpg)';
+        document.body.style.color = "black";
+        titulo.style.color = '#51c153';
+        button.innerHTML = '<h4>Modo oscuro</h4>';
+        texto.style.color = 'white'
+        isBlack = false;
+    } else {
+        document.body.style.background = "#ccc";
+        document.body.style.color = 'black';
+        titulo.style.color = 'black';
+        button.innerHTML = '<h4>Modo claro</h4>';
+        texto.style.color = 'black'
+        
+        isBlack = true;
+    }
+});
+
